9709e2058e498e027fb957065dc592b2ad39d7b0
yogitashete2412 <yogitashete24@gmail.com>
2023-05-07 09:42:27 +0530

    form handling features added

--- src/App.js
+++ src/App.js
@@ -3,26 +3,38 @@ import React,{useState} from 'react'
 function App() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [college, setCollege] = useState('');
 
   return (
     <div>
+      <form>
+
       <h1>Form Handling</h1>
       <label>Name : </label>
       <input type="text" value={name} onChange={(e)=>{
-       setName(e.target.value)
+        setName(e.target.value)
       }}/>
       <br/><br/>
       <label>Email Id : </label>
       <input type="text" value={email} onChange={(e)=>{
-       setEmail(e.target.value)
+        setEmail(e.target.value)
       }}/>
-      <h3>Name: {name}</h3>
-      <h3>Email: {email}</h3>
-     <button type="button">Submit</button>
-     <br/><br/>
-     <button type="button" onClick={()=>{
-      setName("")
-     }}>Clear</button>
+    <br/><br/>
+    <label>College Name: </label>
+    <input type= "text" value={college} onChange={(e)=>{
+       setCollege(e.target.value)
+    }}/>
+       <p>
+         {name} is studying in college {college}.
+       </p>
+
+       
+     {/* <button type="button">Submit</button>
+     <br/><br/> */}
+     {/* <button type="button" onClick={()=>{
+       setName("")
+      }}>Clear</button> */}
+      </form>
     </div>
   )
 }
